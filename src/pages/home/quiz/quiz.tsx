13a8206e9c80515a6303b9e70a882cea5cf1fc0e
Abdurakhmon Ibragimov <apocalypse@Apocalypse-2.local>
2023-11-03 23:03:54 +0500

    finished quiz

--- src/pages/home/quiz/quiz.tsx
+++ src/pages/home/quiz/quiz.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Box, Button, Flex, Text, Title } from '@mantine/core'
+import { Box, Button, Flex, Overlay, Text, Title } from '@mantine/core'
+import { IconArrowLeft } from '@tabler/icons-react'
 import { Api, Types } from 'modules/home'
+import { http } from 'services'
 
 import quizzImg from 'assets/img/Online test-amico.png'
 
@@ -9,41 +11,52 @@ import './quiz.css'
 
 const QuizCategoryPage: React.FC = () => {
   const navigate = useNavigate()
+  const [visible, setVisible] = useState(false)
+  const [correct, setCorrect] = useState(0)
   const [state, setState] = useState<Types.IApi.Question.Response>()
   const { categoryId = 0 } = useParams()
   const [count, setCount] = useState<number>(0)
-  const id = +categoryId
+  const Cid = +categoryId
   const [fullData, setFullData] = useState<Array<any>>([])
   const [isEnd, setIsEnd] = useState(false)
 
   function handleSubmit(id: number) {
     const res = {
-      category: categoryId,
-      answers: [
-        {
-          question_id: state?.questions[count].id,
-          answer_id: id
-        }
-      ]
+      question_id: state?.questions[count].id,
+      answer_id: id
     }
 
     if (state && count < state.questions.length - 1) {
-      setFullData([...fullData, res])
+      setFullData(prevFullData => [...prevFullData, res])
       setCount((count: number) => count + 1)
     } else {
+      setFullData(prevFullData => [...prevFullData, res])
       setIsEnd(true)
       setCount(0)
     }
   }
 
-  function handleEnd() {
-    console.log(fullData)
+  const sendDataToBackend = async () => {
+    try {
+      const ans = {
+        category: Cid,
+        answers: [...fullData]
+      }
+      const response = await http.post('/quizes/answer', ans)
+
+      setCorrect(response.data.is_correct)
+
+      console.log(response.data.is_correct)
+    } catch (error) {
+      console.error('Error sending data to the backend:', error)
+    }
   }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         // @ts-ignore
-        const { data } = await Api.Question(id)
+        const { data } = await Api.Question(Cid)
 
         setState(data)
       } catch (err: any) {
@@ -54,75 +67,104 @@ const QuizCategoryPage: React.FC = () => {
     fetchData()
   }, [])
 
+  const handleSendDataAndOpen = () => {
+    sendDataToBackend()
+    setVisible(v => !v)
+  }
+
   return (
-    <Box>
-      <Flex className="main" align="center" justify="center" wrap="wrap" gap="60px">
-        <img style={{ width: '500px', height: '500px' }} src={quizzImg} alt="img" />
-        <Flex h="100vh" align="center" justify="center">
-          <br />
-          <Box
-            sx={{
-              border: '1px solid white',
-              display: 'grid',
-              borderRadius: '20px',
-              boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px'
-            }}
-            w="350px"
-            key={state?.questions[0].id}
-            p="lg"
-            bg="white"
-          >
-            <Box p="sm" sx={{ display: 'grid', placeItems: 'center' }}>
-              <Title>Question </Title>
-              <Text mt="20px" size="xl" fs="italic">
-                {state?.questions[count].question}
-              </Text>
-            </Box>
-            <hr />
+    <>
+      {visible && (
+        <Overlay color="#000" blur={80}>
+          <Flex w="100%" h="100%" direction="column" align="center" justify="center">
+            <Title color="white" size="150px">
+              You Scored
+            </Title>
+            <Text color="white" size="100px">
+              {correct} /{state && state.questions.length}
+            </Text>
+            <Button onClick={() => navigate('/category')} leftIcon={<IconArrowLeft size={14} />} variant="filled" color="grape">
+              Back Home
+            </Button>
+          </Flex>
+        </Overlay>
+      )}
+      <Box>
+        <Flex className="main" align="center" justify="center" wrap="wrap" gap="60px">
+          <img style={{ width: '500px', height: '500px' }} src={quizzImg} alt="img" />
+          <Flex h="100vh" align="center" justify="center">
             <br />
-            <div>
-              {state?.questions[count].answer.map((item, index) => (
-                <Box key={index} sx={{ display: 'grid' }}>
-                  <Button
-                    variant="transparent"
-                    p="20px"
-                    bg="white"
-                    w="310px"
-                    h="60px"
-                    mt="md"
-                    fullWidth
-                    disabled={isEnd}
-                    sx={{
-                      userSelect: 'none',
-                      cursor: 'pointer',
-                      color: ' #22042e',
-                      borderRadius: '10px',
-                      boxShadow: '#E0E0E0 0px 6px 9px',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-start'
-                    }}
-                    onClick={() => handleSubmit(item.id)}
-                  >
-                    {`${index + 1}. ${item.answer}`}
-                  </Button>
-                </Box>
-              ))}
-              <br />
+            <Box
+              sx={{
+                border: '1px solid white',
+                display: 'grid',
+                borderRadius: '20px',
+                boxShadow: 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px'
+              }}
+              w="350px"
+              key={state?.questions[0].id}
+              p="lg"
+              bg="white"
+            >
+              <Box p="sm" sx={{ display: 'grid', placeItems: 'center' }}>
+                <Title>Question </Title>
+                <Text mt="20px" size="xl" fs="italic">
+                  {state?.questions[count].question}
+                </Text>
+              </Box>
               <hr />
-              <Flex align="center" justify="space-between">
-                <Button w="140px" sx={{ borderRadius: '30px' }} color="green" variant="filled">
-                  Finish Test
-                </Button>
-                <Button w="140px" sx={{ borderRadius: '30px' }} onClick={() => handleEnd()} disabled={!isEnd} color="green" variant="filled">
-                  End
-                </Button>
-              </Flex>
-            </div>
-          </Box>
+              <br />
+              <div>
+                {state?.questions[count].answer.map((item, index) => (
+                  <Box key={index} sx={{ display: 'grid' }}>
+                    <Button
+                      variant="transparent"
+                      p="20px"
+                      bg="white"
+                      w="310px"
+                      h="60px"
+                      mt="md"
+                      fullWidth
+                      disabled={isEnd}
+                      sx={{
+                        userSelect: 'none',
+                        cursor: 'pointer',
+                        color: ' #22042e',
+                        borderRadius: '10px',
+                        boxShadow: '#E0E0E0 0px 6px 9px',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-start'
+                      }}
+                      onClick={() => handleSubmit(item.id)}
+                    >
+                      {`${index + 1}. ${item.answer}`}
+                    </Button>
+                  </Box>
+                ))}
+                <br />
+                <hr />
+                <Flex align="center" justify="space-between">
+                  <Button w="140px" sx={{ borderRadius: '30px' }} onClick={() => handleSendDataAndOpen()} color="green" variant="filled">
+                    Finish Test
+                  </Button>
+                  <Button
+                    w="140px"
+                    sx={{ borderRadius: '30px' }}
+                    onClick={() => handleSendDataAndOpen()}
+                    disabled={!isEnd}
+                    color="green"
+                    variant="filled"
+                  >
+                    End
+                  </Button>
+                </Flex>
+              </div>
+            </Box>
+          </Flex>
         </Flex>
-      </Flex>
-    </Box>
+      </Box>
+    </>
   )
 }
 
